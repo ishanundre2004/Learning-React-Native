189e38acd573cbed36f345c53f8ca31c62b3a04c
Ishan Undre <ishanundre2004@gmail.com>
2024-09-23 21:34:38 +0530

    states and props

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
 
-export default function App() {
+const App = () => {
+  const Click = (val ?: string) =>{
+    console.warn(val);
+  }
+
+  const [name, setName] = useState("Ishan")
+
+  const testUpdate = () => {
+    setName("Pranav")
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {style.container} >
+      <Text style = {style.textstyle} >Hello World !</Text>
+     
+      <UserData name = {name}/>
+      {/* <Button title='Click Me !' onPress={Click} color={'green'}/> */}
+      <Button title='Click Me !' onPress={()=>Click("Hello world")} color={'red'}/>
+      <Button title = "Update state" onPress={testUpdate} color={'blue'}/>
+      <StatusBar style='dark' />
+    </View>
+   
+  );
+};
+
+const UserData = (props) => {
+  return (
+    <View >
+       <Text> Name :{props.name} Undre</Text>
+      <Text> Age : 20</Text>
+      <Text> Email : ishanundre2004@gmail.com </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+const style = StyleSheet.create({
+  container : {
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center',
   },
-});
+  textstyle : {
+    fontSize : 30,
+  }
+})
+
+export default App;
