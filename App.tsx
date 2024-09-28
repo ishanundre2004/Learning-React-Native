85640f18c5fc0d473dfec8ee5a93037c5368cf8d
Ishan Undre <ishanundre2004@gmail.com>
2024-09-28 20:51:39 +0530

    user input

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import React, {useState} from 'react';
 
 const App = () => {
@@ -7,34 +7,42 @@ const App = () => {
     console.warn(val);
   }
 
-  const [name, setName] = useState("Ishan")
+  const [name, setName] = useState('')
 
-  const testUpdate = () => {
-    setName("Pranav")
-  }
+  // const testUpdate = () => {
+  //   setName("Pranav")
+  // }
   return (
     <View style = {style.container} >
-      <Text style = {style.textstyle} >Hello World !</Text>
+      {/* <Text style = {style.textstyle} >Hello World !</Text> */}
      
-      <UserData name = {name}/>
+      {/* <UserData name = {name}/> */}
       {/* <Button title='Click Me !' onPress={Click} color={'green'}/> */}
-      <Button title='Click Me !' onPress={()=>Click("Hello world")} color={'red'}/>
-      <Button title = "Update state" onPress={testUpdate} color={'blue'}/>
+      {/* <Button title='Click Me !' onPress={()=>Click("Hello world")} color={'red'}/> */}
+      {/* <Button title = "Update state" onPress={testUpdate} color={'blue'}/> */}
+      {/* <Text style ={style.textstyle}>Hi {name}</Text>
+      <TextInput 
+        style = {style.textinput}
+        placeholder='Enter your name'
+        onChangeText={(text)=>setName(text)}
+        /> */}
+
+      
       <StatusBar style='dark' />
     </View>
    
   );
 };
 
-const UserData = (props) => {
-  return (
-    <View >
-       <Text> Name :{props.name} Undre</Text>
-      <Text> Age : 20</Text>
-      <Text> Email : ishanundre2004@gmail.com </Text>
-    </View>
-  );
-}
+// const UserData = (props) => {
+//   return (
+//     <View >
+//        <Text> Name :{props.name} Undre</Text>
+//       <Text> Age : 20</Text>
+//       <Text> Email : ishanundre2004@gmail.com </Text>
+//     </View>
+//   );
+// }
 
 
 const style = StyleSheet.create({
@@ -45,6 +53,14 @@ const style = StyleSheet.create({
   },
   textstyle : {
     fontSize : 30,
+  }, 
+  textinput : {
+    margin: 10,
+    fontSize :15,
+    borderColor :'blue',
+    borderWidth : 2,
+    width : 300,
+    padding :10,
   }
 })
 
